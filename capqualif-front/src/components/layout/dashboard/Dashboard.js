ee35e2f873e8cd27d4f10d19ff095d6e05911787
Elisabeth Fainstein <elisabeth.fainstein@developpement-durable.gouv.fr>
2020-11-04 23:15:39 +0100

    add grid to dashboard

--- capqualif-front/src/components/layout/dashboard/Dashboard.js
+++ capqualif-front/src/components/layout/dashboard/Dashboard.js
@@ -10,6 +10,9 @@ const Dashboard = () => {
     return (
         <div id="dashboard">
             {/* <h2>MON TABLEAU DE BORD</h2> */}
+            <div id="greetings">
+                <h3>Bonjour, {store.getState().sailors.sailorCivilData.firstName} !</h3>
+            </div>
             <nav>
                 <div id="photo">
                     <img src={sailorPicMock} alt="a fake sailor's pic"/>
@@ -18,32 +21,32 @@ const Dashboard = () => {
                     <img src={menuPicMock}/>
                 </div>
             </nav>
-            <div id="greetings">
-                <h3>Bonjour, {store.getState().sailors.sailorCivilData.firstName} !</h3>
-            </div>
-            <div id="titles">
-                <button id="ask-for-a-title">
-                    Demander un titre
-                </button>
-                <div id="my-titles">
-                    <div id="main-title">
-                        <p className="title">Mon titre principal</p>
-                        <div className="card">Matelot Pont</div>
-                    </div>
-                    <div id="other-titles">
-                        <p className="title">Mes autres titres</p>
-                        <div className="card">Matelot Pont</div>
-                        <div className="card">Matelot Pont</div>
-                    </div>
-                </div>
-            </div>
+            <button id="ask-for-a-title">
+                Demander un titre
+            </button>
             <div id="applications-in-progress">
                 <p className="title">Mes demandes en cours</p>
-                <ul>
-                    <li>Matelot Pont</li>
-                    <li>CFBS</li>
+                <ul className="card">
+                    <li>CFBS : dossier en traitement ⏳</li>
                     <li>Premiers secours</li>
                 </ul>
+            </div>
+            <div id="main-title">
+                <p className="title">Mon titre principal</p>
+                <div className="card">Matelot Pont</div>
+            </div>
+            <div id="other-titles">
+                <p className="title">Mes autres titres</p>
+                <div id="other-titles-container">
+                    <div className="card other-titles-card">Matelot Pont</div>
+                    <div className="card other-titles-card">Matelot Pont</div>
+                    <div className="card other-titles-card">Matelot Pont</div>
+                    <div className="card other-titles-card">Matelot Pont</div>
+                </div>
+
+            </div>
+            <div id="service-lines">
+                Mes lignes de service
             </div>
         </div>
     );
